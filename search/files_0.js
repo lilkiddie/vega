3b518ff46685a1ce0f884080c781e5a60fa57114
lilkiddie <lilkiddie@users.noreply.github.com>
2024-10-10 18:27:23 +0000

    deploy: f7918ca43bf40ac5b66b0c87dd1fbfd55e7dc337

---
+++ search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['my_5fadd_2ecpp_3',['my_add.cpp',['../my__add_8cpp.html',1,'']]],
+  ['my_5fadd_2ehpp_4',['my_add.hpp',['../my__add_8hpp.html',1,'']]]
+];
---
+++ search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['my_5fadd_5',['my_add',['../my__add_8hpp.html#ab00928055ac944ac4f073dd731e0feff',1,'my_add(int, int):&#160;my_add.cpp'],['../my__add_8cpp.html#addd044fcb17df514182a0809318edc31',1,'my_add(int a, int b):&#160;my_add.cpp']]]
+];
