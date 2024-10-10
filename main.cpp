29f42d1317b656a16cb847ae138a39eaadc29239
lilkiddie <lmd.2002@mail.ru>
2024-10-10 18:57:59 +0300

    add cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-    std::cout << "Hello world" << std::endl;
+#include "my_add.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        std::cerr << "Provide 2 arguments" << std::endl;
+        return 1;
+    }
+    int a = std::atoi(argv[1]);
+    int b = std::atoi(argv[2]);
+    std::cout << my_add(a, b) << std::endl;
     return 0;
 }
